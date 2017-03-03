136c5abb01d5277b1bf4ced7cd6da4a15e449561
Joshua Chini <JoshuaCode@user.noreply.github.com>
2017-03-03 10:28:27 -0500

    Initial commit of sample application

---
+++ EntityFrameworkCoreOracle/Person.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkCoreOracle
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Scid { get; set; }
+        public string ManagerScid { get; set; }
+        public virtual Person Manager { get; set; }
+        public virtual ICollection<Person> DirectReports { get; set; }
+
+    }
+
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle(@"Data Source=<DATA_SOURCE>;User ID=<USER_ID>;Password=<PASSWORD>;");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("PersonSchema");
+            modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.HasKey(b => b.Id);
+
+                entity.Property(b => b.Name)
+                .HasColumnName("NAME");
+
+                entity.Property(b => b.Scid)
+                    .HasColumnName("SCID");
+
+                entity.Property(b => b.ManagerScid)
+                .HasColumnName("REPORTS_TO_SCID");
+
+                entity.HasOne(b => b.Manager)
+                .WithMany(b => b.DirectReports)
+                .HasForeignKey(e => e.ManagerScid)
+                .HasPrincipalKey(b => b.Scid);
+            });
+        }
+    }
+}
---
+++ EntityFrameworkCoreOracle/Program.cs
@@ -0,0 +1,45 @@
+﻿using Devart.Data.Oracle.Entity.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkCoreOracle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = OracleEntityProviderConfig.Instance;
+            config.Workarounds.DisableQuoting = true;
+
+            using (var db = new PersonContext())
+            {
+                Console.WriteLine("### Query 1");
+                var persons = db.Persons.Include(person => person.Manager);
+                foreach (var person in persons)
+                {
+                    Console.WriteLine($"Scid: {person.Scid} Name: {person.Name}  Manager: {person.Manager?.Name}");
+                }
+                Console.WriteLine("### Query 2");
+                var personsList = db.Persons.Select(p => new Person()
+                {
+                    Name = p.Name,
+                    Manager = new Person()
+                    {
+                        Name = p.Manager.Name
+                    }
+                });
+                foreach (var person in personsList)
+                {
+                    Console.WriteLine($"Scid: {person.Scid} Name: {person.Name}  Manager: {person.Manager?.Name}");
+                }
+                Console.WriteLine("### Query 3");
+                var directReports = db.Persons.Where(p => p.Scid == "00000001").Select(p => new Person() { Name = p.Name, DirectReports = p.DirectReports }).FirstOrDefault();
+                foreach (var person in directReports.DirectReports)
+                {
+                    Console.WriteLine($"Manager: {directReports.Name} EmployeeName: {person.Name}");
+                }
+            }
+        }
+    }
+}
